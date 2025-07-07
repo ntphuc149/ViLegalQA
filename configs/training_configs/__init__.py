d4232bbee2bf93004191d40fb96c0f22703c9d59
Truong-Phuc Nguyen <99306941+ntphuc149@users.noreply.github.com>
2025-07-07 19:06:16 +0700

    Delete configs directory

--- configs/training_configs/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Training-specific configuration classes for ViBidLQA-AQA
-"""
-
-from .finetune_config import FinetuneConfig
-from .instruct_config import InstructConfig
-
-__all__ = ["FinetuneConfig", "InstructConfig"]
